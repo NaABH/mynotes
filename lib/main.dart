e32ebbb82588798c576568b57b59b16a502914c6
naabh <nanhw2002@gmail.com>
2023-09-26 15:37:29 +0800

    step-7

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/views/login_view.dart';
 import 'package:mynotes/views/register_view.dart';
-import 'package:mynotes/views/verify_emai_view.dart';
+import 'package:mynotes/views/verify_email_view.dart';
 import 'constants/routes.dart';
 import 'firebase_options.dart';
 
@@ -21,6 +21,7 @@ void main() {
         loginRoute: (context) => const LoginView(),
         registerRoute: (context) => const RegisterView(),
         notesRoute: (context) => const NotesView(),
+        verifyEmailRoute: (context) => const VerifyEmailView(),
       },
     ),
   );
